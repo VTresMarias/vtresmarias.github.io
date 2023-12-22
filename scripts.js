5c7810d9e5f8a8f008cce49d5d80f71b0d42afa9
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-12-22 21:17:31 +0800

    hover fix following broken images

--- scripts.js
+++ scripts.js
@@ -7,26 +7,26 @@ function lotusLoading() {
 
 //code from https://linuxhint.com/change-image-on-hover-in-javascript 
 function aura_hover() {
-  document.getElementById("aura-hover").src = "vtm_aura_hover.svg";
+  document.getElementById("aura-hover").src = "https://vtresmarias.github.io/vtm_aura_hover.svg";
   document.getElementById("aura-hover").style.transition = "0.5s";
 }
 function aura_hoverOut() {
-  document.getElementById("aura-hover").src = "vtm_aura.svg";
+  document.getElementById("aura-hover").src = "https://vtresmarias.github.io/vtm_aura.svg";
   document.getElementById("aura-hover").style.transition = "0.5s";
 }
 function aga_hover() {
-  document.getElementById("aga-hover").src = "vtm_aga_hover.svg";
+  document.getElementById("aga-hover").src = "https://vtresmarias.github.io/vtm_aga_hover.svg";
   document.getElementById("aga-hover").style.transition = "0.5s";
 }
 function aga_hoverOut() {
-  document.getElementById("aga-hover").src = "vtm_aga.svg";
+  document.getElementById("aga-hover").src = "https://vtresmarias.github.io/vtm_aga.svg";
   document.getElementById("aga-hover").style.transition = "0.5s";
 }
 function hina_hover() {
-  document.getElementById("hina-hover").src = "vtm_hina_hover.svg";
+  document.getElementById("hina-hover").src = "https://vtresmarias.github.io/vtm_hina_hover.svg";
   document.getElementById("hina-hover").style.transition = "0.5s";
 }
 function hina_hoverOut() {
-  document.getElementById("hina-hover").src = "vtm_hina.svg";
+  document.getElementById("hina-hover").src = "https://vtresmarias.github.io/vtm_hina.svg";
   document.getElementById("hina-hover").style.transition = "0.5s";
 }
