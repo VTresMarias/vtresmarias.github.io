c315c841fd84d3c4611cff7879e5f936f1608cef
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2024-04-12 14:29:43 +0800

    more stuff

--- pwa/scripts.js
+++ pwa/scripts.js
@@ -1,13 +1,15 @@
 // detect if site is WebApp
 
-const parseQuery = new URLSearchParams(window.location.search);
-const isWebApp = parseQuery.get("isWebApp");
-if (isWebApp == "1") {
-  console.log("this is a webapp");
-  defineMaria();
-} else {
-  // console.log("this is NOT a webapp");
-  window.location.replace("NotAPWA");
+const parseQuery = new URLSearchParams(window.location.search); // global value
+
+function isWebApp() {
+  const isWebApp = parseQuery.get("isWebApp");
+  if (isWebApp == "1") {
+    console.log("this is a webapp");
+  } else {
+    // console.log("this is NOT a webapp");
+    window.location.replace("NotAPWA");
+  }
 }
 // code: https://sitepoint.com/get-url-parameters-with-javascript 
 
@@ -18,11 +20,32 @@ function defineMaria() {
   const mariaDef = parseQuery.get("mariaDef");
   if (mariaDef == "1") {
     console.log("First Maria");
+    document.getElementById("mariaDeleg").innerHTML = "First Maria🍃";
+    document.getElementById("blurStuff").style.filter = "blur(.5em)";
+    document.getElementById("AreYouSure").style.display = "block";
+    document.getElementById("mariaCont").setAttribute("onclick", "window.open('https://thebelovedmoon.github.io')");
+    // setAttribute answer: https://stackoverflow.com/a/4689368 
   } else if (mariaDef == "2") {
     console.log("Second Maria");
+    document.getElementById("mariaDeleg").innerHTML = "Second Maria🪷";
+    document.getElementById("blurStuff").style.filter = "blur(.5em)";
+    document.getElementById("AreYouSure").style.display = "block";
+    document.getElementById("mariaCont").setAttribute("onclick", "window.open('https://thebelovedmoon.github.io/auraostara')");
   } else if (mariaDef == "3") {
     console.log("Third Maria");
+    document.getElementById("mariaDeleg").innerHTML = "Third Maria🌸";
+    document.getElementById("blurStuff").style.filter = "blur(.5em)";
+    document.getElementById("AreYouSure").style.display = "block";
+    document.getElementById("mariaCont").setAttribute("onclick", "window.open('https://thebelovedmoon.github.io/hinaoujo')");
   } else {
     console.log("undefined");
   }
+}
+
+
+// back to page
+
+function backToPage() {
+  document.getElementById("blurStuff").style.filter = "unset";
+  document.getElementById("AreYouSure").style.display = "none";
 }
