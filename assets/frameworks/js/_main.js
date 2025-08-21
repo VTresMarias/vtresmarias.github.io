686f0365e61f8ee91107f66b9c6493f1d0ba5cc1
💚🍃Aga-chuu💗🌸 <103877195+thebelovedmoon@users.noreply.github.com>
2025-08-21 13:43:35 +0000

    js cleanup and replace

--- assets/frameworks/js/_main.js
+++ assets/frameworks/js/_main.js
@@ -7,10 +7,10 @@ function init() {
 
 function removeSplash() {
   setTimeout(() => {
-    document.getElementById("loaderFade").style.opacity = "0";
+    document.getElementById("loaderFade").setAttribute("style", "opacity: 0;");
     setTimeout(() => {
-      document.getElementById("loaderFade").style.display = "none";
-      document.getElementById("loadBlock").style.height = "0";
+      document.getElementById("loaderFade").setAttribute("style", "display: none;");
+      document.getElementById("loadBlock").setAttribute("style", "height: 0;");
     }, 1250);
   }, 1250);
 }
